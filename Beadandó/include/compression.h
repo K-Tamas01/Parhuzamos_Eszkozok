5069143466f16e6d0f3366d10bda60919b34231b
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-05-20 19:21:36 +0200

    addd struct header

--- Beadandó/include/compression.h
+++ Beadandó/include/compression.h
@@ -1,14 +1,8 @@
 #ifndef COMPRESSION_H
 #define COMPRESSION_H
 
-/**
- * Load the OpenCL kernel source code from a file.
- * 
- * path: Path of the source file
- * error_code: 0 on successful file loading
- * 
- * Returns by a dynamically allocated string
- */
-int compreession(char* route, int* data);
+#include "structHeader.h"
+
+int compreession(char* route, int* data, char* characters, int fSize);
 
 #endif
